84955a99642e4777d67aa4e4a51e9c6d02de279c
Johan Larsson <johan_gavle1@hotmail.com>
2014-11-28 19:23:17 +0100

    WIP adorners

--- Gu.Wpf.Reactive/TouchToolTip.cs
+++ Gu.Wpf.Reactive/TouchToolTip.cs
@@ -1,9 +1,44 @@
 ﻿namespace Gu.Wpf.Reactive
 {
+    using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Data;
 
+    /// <summary>
+    /// Exposes AdornedElement and sets DataContext to 
+    /// </summary>
     public class TouchToolTip : ToolTip
     {
+        public static readonly DependencyProperty AdornedElementProperty = DependencyProperty.Register(
+            "AdornedElement",
+            typeof(FrameworkElement),
+            typeof(TouchToolTip),
+            new PropertyMetadata(default(FrameworkElement), OnAdornedElementChanged));
 
+        public FrameworkElement AdornedElement
+        {
+            get
+            {
+                return (FrameworkElement)GetValue(AdornedElementProperty);
+            }
+            set
+            {
+                SetValue(AdornedElementProperty, value);
+            }
+        }
+
+        private static void OnAdornedElementChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var frameworkElement = e.NewValue as FrameworkElement;
+            if (frameworkElement != null)
+            {
+                var binding = new Binding(DataContextProperty.Name)
+                                  {
+                                      Mode = BindingMode.OneWay, 
+                                      Source = frameworkElement
+                                  };
+                BindingOperations.SetBinding(o, DataContextProperty, binding);
+            }
+        }
     }
 }
