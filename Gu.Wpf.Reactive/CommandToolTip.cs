b71f163a5705b20e5e87f9e03cd134f8bd157a60
Johan Larsson <johan_gavle1@hotmail.com>
2014-11-29 16:34:42 +0100

    Moved tooltips to separate project

--- Gu.Wpf.Reactive/CommandToolTip.cs
+++ Gu.Wpf.Reactive/CommandToolTip.cs
@@ -5,49 +5,31 @@
     using System.Windows.Controls.Primitives;
     using System.Windows.Data;
 
+    using Gu.Wpf.ToolTips;
+
     /// <summary>
     /// Exposes AdornedElement and sets DataContext to the Command of the adorned element
     /// </summary>
-    public class CommandToolTip : ToolTip
+    public class CommandToolTip : ToolTip, ITouchToolTip
     {
-        /// <summary>
-        /// Exposong the adorned element for convenience
-        /// </summary>
-        public static readonly DependencyProperty AdornedElementProperty = TouchToolTip.AdornedElementProperty.AddOwner(
-            typeof(CommandToolTip),
-            new PropertyMetadata(
-                default(FrameworkElement),
-                OnAdornedElementChanged));
-
         static CommandToolTip()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CommandToolTip), new FrameworkPropertyMetadata(typeof(CommandToolTip)));
         }
 
-        public FrameworkElement AdornedElement
+        public void OnToolTipChanged(UIElement adornedElement)
         {
-            get
-            {
-                return (FrameworkElement)GetValue(AdornedElementProperty);
-            }
-            set
-            {
-                SetValue(AdornedElementProperty, value);
-            }
-        }
-
-        private static void OnAdornedElementChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
-        {
-            var frameworkElement = e.NewValue as FrameworkElement;
-            if (frameworkElement != null)
-            {
-                var binding = new Binding(ButtonBase.CommandProperty.Name)
-                {
-                    Mode = BindingMode.OneWay,
-                    Source = frameworkElement
-                };
-                BindingOperations.SetBinding(o, DataContextProperty, binding);
-            }
+            throw new System.NotImplementedException();
+            //var frameworkElement = e.NewValue as FrameworkElement;
+            //if (frameworkElement != null)
+            //{
+            //    var binding = new Binding(ButtonBase.CommandProperty.Name)
+            //    {
+            //        Mode = BindingMode.OneWay,
+            //        Source = frameworkElement
+            //    };
+            //    BindingOperations.SetBinding(o, DataContextProperty, binding);
+            //}
         }
     }
 }
