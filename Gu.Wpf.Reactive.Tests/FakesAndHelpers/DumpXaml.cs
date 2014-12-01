b74c005b089d0e7b852aaeff2c2b8903d3bd7523
Johan Larsson <johan_gavle1@hotmail.com>
2014-12-01 19:13:48 +0100

    WIP

---
+++ Gu.Wpf.Reactive.Tests/FakesAndHelpers/DumpXaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Gu.Wpf.Reactive.Tests.FakesAndHelpers
+{
+    using System.Xml.Linq;
+
+    using NUnit.Framework;
+
+    public class DumpXaml
+    {
+        [Test]
+        public void DumpTaskCompletion()
+        {
+            var propertyInfos = typeof(NotifyTaskCompletion).GetProperties();
+            var grid = new XElement( "Grid");
+            var rowDefs = new XElement("RowDefinitions");
+            grid.Add(rowDefs);
+        }
+    }
+}
