7cf1fb06d43552bab4233c3e0848816169529ead
Johan Larsson <johan_gavle1@hotmail.com>
2014-12-01 09:19:44 +0100

    WIP -> work

---
+++ Gu.Wpf.Reactive/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gu.Wpf.Reactive
+{
+    using System.ComponentModel;
+    using System.Windows.Input;
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync(object parameter);
+    }
+}
