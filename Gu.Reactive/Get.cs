ece4701e5d0296e56e92e73780c66232e970d8b8
Johan Larsson <johan_gavle1@hotmail.com>
2015-03-30 23:34:36 +0200

    WIP

--- Gu.Reactive/Get.cs
+++ Gu.Reactive/Get.cs
@@ -1,12 +1,8 @@
 ﻿namespace Gu.Reactive
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Linq.Expressions;
     using System.Reflection;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public static class Get
     {
