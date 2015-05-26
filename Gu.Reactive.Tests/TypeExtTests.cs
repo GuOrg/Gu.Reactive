cf6d99251ef2472b5a46eba2606674025884a3d6
Johan Larsson <johan_gavle1@hotmail.com>
2015-05-26 15:23:26 +0200

    A couple of tests for TypeExtTests

---
+++ Gu.Reactive.Tests/TypeExtTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Gu.Reactive.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+
+    public class TypeExtTests
+    {
+        [TestCase(typeof(double), "double")]
+        [TestCase(typeof(Nullable<double>), "Nullable<double>")]
+        [TestCase(typeof(IDictionary<string, double>), "IDictionary<string, double>")]
+        public void PrettyName(Type type, string expected)
+        {
+            var actual = type.PrettyName();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Gu.Wpf.Reactive.Tests/Converters/TypeConverters/DefaultTypeConverter`1Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace Gu.Wpf.Reactive.Tests.Converters.TypeConverters
+{
+    using System;
+    using NUnit.Framework;
+    using Reactive.TypeConverters;
+
+    public class DefaultTypeConverterTests
+    {
+        [TestCase(null, true)]
+        [TestCase(true, true)]
+        [TestCase(2, false)]
+        [TestCase("", false)]
+        public void IsValidNullable_Bool(object value, bool expected)
+        {
+            var converter = TypeConverterFactory.Create<Nullable<bool>>();
+            var actual = converter.IsValid(value);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(null, true)]
+        [TestCase(1.0, true)]
+        [TestCase(2, true)]
+        [TestCase("", false)]
+        public void IsValidNullable_Double(object value, bool expected)
+        {
+            var converter = TypeConverterFactory.Create<Nullable<double>>();
+            var actual = converter.IsValid(value);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
