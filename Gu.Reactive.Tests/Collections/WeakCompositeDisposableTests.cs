7f42a360ba2ad78463f12112f2982ed5e8f68218
Johan Larsson <johan_gavle1@hotmail.com>
2015-07-11 18:54:46 +0200

    Test for CompositeDisposable

---
+++ Gu.Reactive.Tests/Collections/WeakCompositeDisposableTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Gu.Reactive.Tests.Collections
+{
+    using System;
+    using Moq;
+    using NUnit.Framework;
+
+    public class WeakCompositeDisposableTests
+    {
+        [Test]
+        public void Disposes()
+        {
+            var compositeDisposable = new WeakCompositeDisposable();
+            var mock1 = new Mock<IDisposable>();
+            var mock2 = new Mock<IDisposable>();
+            compositeDisposable.Add(mock1.Object);
+            compositeDisposable.Add(mock2.Object);
+            compositeDisposable.Dispose();
+            mock1.Verify(x => x.Dispose(), Times.Once);
+            mock2.Verify(x => x.Dispose(), Times.Once);
+        }
+    }
+}
