a5f59890377dc88eda6b957e2d2f6c86a2067feb
Johan Larsson <johan_gavle1@hotmail.com>
2017-01-04 12:26:19 +0100

    Docs FixedSizedQueue

--- Gu.Reactive/Collections/FixedSizedQueue{T}.cs
+++ Gu.Reactive/Collections/FixedSizedQueue{T}.cs
@@ -6,30 +6,49 @@
     using System.Collections.Generic;
     using System.Diagnostics;
 
+    /// <summary>
+    /// A fixed size queue. Overflow is trimmed when adding more items than max.
+    /// Wraps a <see cref="ConcurrentQueue{T}"/>
+    /// </summary>
     [Serializable]
     [DebuggerTypeProxy(typeof(CollectionDebugView<>))]
+    //// ReSharper disable once UseNameofExpression
     [DebuggerDisplay("Count = {Count}")]
     public class FixedSizedQueue<T> : IProducerConsumerCollection<T>
     {
         private readonly ConcurrentQueue<T> innerQueue = new ConcurrentQueue<T>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FixedSizedQueue{T}"/> class.
+        /// </summary>
+        /// <param name="size">The maximum number of elements in the queue.</param>
         public FixedSizedQueue(int size)
         {
             this.Size = size;
         }
 
+        /// <summary>
+        /// The maximum number of items.
+        /// </summary>
         public int Size { get; }
 
+        /// <inheritdoc/>
         public int Count => this.innerQueue.Count;
 
+        /// <inheritdoc/>
         object ICollection.SyncRoot => ((ICollection)this.innerQueue).SyncRoot;
 
+        /// <inheritdoc/>
         bool ICollection.IsSynchronized => ((ICollection)this.innerQueue).IsSynchronized;
 
+        /// <inheritdoc/>
         public IEnumerator<T> GetEnumerator() => this.innerQueue.GetEnumerator();
 
+        /// <inheritdoc/>
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 
+        /// <summary>Adds an object to the end of the <see cref="FixedSizedQueue{T}"/> overflow is termmed off.</summary>
+        /// <param name="item">The object to add to the end of the <see cref="FixedSizedQueue{T}"/>. The value can be a null reference (Nothing in Visual Basic) for reference types.</param>
         public virtual void Enqueue(T item)
         {
             this.innerQueue.Enqueue(item);
@@ -45,16 +64,29 @@
             }
         }
 
+        /// <inheritdoc/>
         public void CopyTo(Array array, int index) => ((ICollection)this.innerQueue).CopyTo(array, index);
 
+        /// <inheritdoc/>
         public void CopyTo(T[] array, int index) => this.innerQueue.CopyTo(array, index);
 
+        /// <inheritdoc/>
         public bool TryAdd(T item) => ((IProducerConsumerCollection<T>)this.innerQueue).TryAdd(item);
 
+        /// <inheritdoc/>
         public bool TryTake(out T item) => ((IProducerConsumerCollection<T>)this.innerQueue).TryTake(out item);
 
+        /// <inheritdoc/>
         public T[] ToArray() => this.innerQueue.ToArray();
 
-        protected bool TryDequeue(out T overflow) => this.innerQueue.TryDequeue(out overflow);
+        /// <summary>Tries to return an object from the beginning of the <see cref="T:System.Collections.Concurrent.ConcurrentQueue`1" /> without removing it.</summary>
+        /// <returns>true if an object was returned successfully; otherwise, false.</returns>
+        /// <param name="result">When this method returns, <paramref name="result" /> contains an object from the beginning of the <see cref="T:System.Collections.Concurrent.ConcurrentQueue`1" /> or an unspecified value if the operation failed.</param>
+        public bool TryPeek(out T result) => this.innerQueue.TryPeek(out result);
+
+        /// <summary>Tries to remove and return the object at the beginning of the concurrent queue.</summary>
+        /// <returns>true if an element was removed and returned from the beginning of the <see cref="T:System.Collections.Concurrent.ConcurrentQueue`1" /> successfully; otherwise, false.</returns>
+        /// <param name="result">When this method returns, if the operation was successful, <paramref name="result" /> contains the object removed. If no object was available to be removed, the value is unspecified.</param>
+        protected bool TryDequeue(out T result) => this.innerQueue.TryDequeue(out result);
     }
 }
