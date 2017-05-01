6878924ed16f5c2e383aa988be065baacd2d0eba
Johan Larsson <johan_gavle1@hotmail.com>
2017-05-02 00:41:40 +0200

    WIP

---
+++ Gu.Reactive.Analyzers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gu.Reactive.Analyzers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gu.Reactive.Analyzers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f154810d-9d97-4efe-8a86-1312a780802c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Gu.Reactive.Analyzers/GU0001DontSubscribeToMutableProperty.cs
@@ -0,0 +1,49 @@
+﻿namespace Gu.Reactive.Analyzers
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class GU0001DontSubscribeToMutableProperty : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "GU0001";
+        private const string Title = "Ignore events when serializing.";
+        private const string MessageFormat = "Ignore events when serializing.";
+        private const string Description = "Ignore events when serializing.";
+        private static readonly string HelpLink = @"https://www.google.com";
+
+        private static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
+            id: DiagnosticId,
+            title: Title,
+            messageFormat: MessageFormat,
+            category: AnalyzerCategory.Correctness,
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: Description,
+            helpLinkUri: HelpLink);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(HandleInvocation, SyntaxKind.InvocationExpression);
+        }
+
+        private static void HandleInvocation(SyntaxNodeAnalysisContext context)
+        {
+            //if (context.IsExcludedFromAnalysis())
+            //{
+            //    return;
+            //}
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, context.Node.GetLocation()));
+        }
+    }
+}
