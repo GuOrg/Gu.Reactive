991ce8eee294cafb64ee2ff612736e0de893d536
JohanLarsson <johan_gavle1@hotmail.com>
2017-05-15 11:41:01 +0200

    Bump version.

---
+++ Gu.Reactive.Analyzers.CodeFixes/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gu.Reactive.Analyzers.CodeFixes.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Johan Larsson")]
+[assembly: AssemblyProduct("Gu.Reactive.Analyzers.CodeFixes.Properties")]
+[assembly: AssemblyCopyright("Copyright © Johan Larsson 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1f09add9-2a7a-426e-a166-79423fb45944")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ Gu.Reactive.Analyzers/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gu.Reactive.Analyzers.Properties")]
+[assembly: AssemblyDescription("Roslyn analyzers for Gu.Reactive")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Johan Larsson")]
+[assembly: AssemblyProduct("Gu.Reactive.Analyzers.Properties")]
+[assembly: AssemblyCopyright("Copyright © Johan Larsson 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6c9f9c93-09e1-4b56-aedb-cc4d34053a5c")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
