7b98caa19600e14f8ad4436e67f00d0d017f2225
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-09 14:32:45 +0200

    Add test clicking tabs in mainwindow.

---
+++ Gu.Wpf.Reactive.UiTests/MainWindowTests.cs
@@ -0,0 +1,24 @@
+namespace Gu.Wpf.Reactive.UiTests
+{
+    using Gu.Wpf.UiAutomation;
+    using NUnit.Framework;
+
+    public class MainWindowTests
+    {
+        [Test]
+        public void ClickAllTabs()
+        {
+            // Just a smoke test so that we do not explode.
+            using (var app = Application.Launch(Info.ExeFileName))
+            {
+                var window = app.MainWindow;
+                Assert.AreEqual("MainWindow", window.Title);
+                var tab = window.FindTabControl();
+                foreach (var tabItem in tab.Items)
+                {
+                    tabItem.Click();
+                }
+            }
+        }
+    }
+}
