70f19ed280330a76e8308b48d37ea4a42c23ead2
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-23 14:51:13 +0200

    Sign.

--- Gu.Wpf.Reactive/Properties/AssemblyInfo.cs
+++ Gu.Wpf.Reactive/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Windows.Markup;
 [assembly: AssemblyFileVersion("3.4.3.0")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: Guid("1E55FADA-8E21-45DC-B416-224C1956AD39")]
-[assembly: InternalsVisibleTo("Gu.Wpf.Reactive.Tests", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Gu.Wpf.Reactive.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100AB5BC1D032E9E344649AF43E93555B9F69733DCB59951999FEF8A5ADAB5985D5D27DFC91615F062B1AE47FACC549F07E182E0D1F247D587DB83ED87524E18E806336EAEAEB87271C60C790693A3BD6A199D896DD8ED367FD3B3E350B1B0DD78F8BD212872805939C337D96F104DAF36B79ABCEB6D3C7F0F951CC9880A66846AB", AllInternalsVisible = true)]
 
 #pragma warning disable WPF0052 // XmlnsDefinitions does not map all namespaces with public types.
 #pragma warning disable WPF0051 // XmlnsDefinition must map to existing namespace.
