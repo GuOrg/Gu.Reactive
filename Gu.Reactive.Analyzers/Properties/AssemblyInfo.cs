280f279facc9a1699d2e0ea7f0e4956e6363b155
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-13 12:59:06 +0200

    Bump version analyzer.

--- Gu.Reactive.Analyzers/Properties/AssemblyInfo.cs
+++ Gu.Reactive.Analyzers/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Gu.Reactive.Analyzers.Properties")]
+[assembly: AssemblyTitle("Gu.Reactive.Analyzers")]
 [assembly: AssemblyDescription("Roslyn analyzers for Gu.Reactive")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Johan Larsson")]
-[assembly: AssemblyProduct("Gu.Reactive.Analyzers.Properties")]
+[assembly: AssemblyProduct("Gu.Reactive.Analyzers")]
 [assembly: AssemblyCopyright("Copyright © Johan Larsson 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6c9f9c93-09e1-4b56-aedb-cc4d34053a5c")]
-[assembly: AssemblyVersion("0.1.2.3")]
-[assembly: AssemblyFileVersion("0.1.2.3")]
+[assembly: AssemblyVersion("0.1.2.4")]
+[assembly: AssemblyFileVersion("0.1.2.4")]
