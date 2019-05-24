d71e4bf61845ab801a1b2daef7fcbb93908cdf75
Johan Larsson <johan_gavle1@hotmail.com>
2019-05-24 23:28:36 +0200

    Observable.Pair()

---
+++ Gu.Reactive.Tests/ObservableExtTests.Pair.cs
@@ -0,0 +1,36 @@
+namespace Gu.Reactive.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reactive.Subjects;
+    using NUnit.Framework;
+
+    public partial class ObservableExtTests
+    {
+        public class Pair
+        {
+            [Test]
+            public void Sequence()
+            {
+                using (var subject = new Subject<int>())
+                {
+                    var actuals = new List<Paired<int>>();
+                    using (subject.Pair().Subscribe(x => actuals.Add(x)))
+                    {
+                        CollectionAssert.IsEmpty(actuals);
+
+                        subject.OnNext(1);
+                        CollectionAssert.IsEmpty(actuals);
+
+                        subject.OnNext(2);
+                        CollectionAssert.AreEqual(new[] { "1,2" }, actuals.Select(x => $"{x.Previous},{x.Current}"));
+
+                        subject.OnNext(3);
+                        CollectionAssert.AreEqual(new[] { "1,2", "2,3" }, actuals.Select(x => $"{x.Previous},{x.Current}"));
+                    }
+                }
+            }
+        }
+    }
+}
