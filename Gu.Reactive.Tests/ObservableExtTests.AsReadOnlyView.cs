50ae8a7391412fe09658ee87a8bb24f2aabec46b
JohanLarsson <johan_gavle1@hotmail.com>
2019-09-07 13:23:02 +0200

    More tests.
    #76.

--- Gu.Reactive.Tests/ObservableExtTests.AsReadOnlyView.cs
+++ Gu.Reactive.Tests/ObservableExtTests.AsReadOnlyView.cs
@@ -2,6 +2,7 @@ namespace Gu.Reactive.Tests
 {
     using System;
     using System.Collections.Generic;
+    using System.Reactive.Linq;
     using System.Reactive.Subjects;
 
     using Gu.Reactive.Tests.Helpers;
@@ -53,6 +54,40 @@ namespace Gu.Reactive.Tests
             {
                 var with = new With<IEnumerable<int>>();
                 using (var view = with.ObserveValue(x => x.Value).AsReadOnlyView())
+                {
+                    using (var actual = view.SubscribeAll())
+                    {
+                        with.Value = new[] { 1 };
+                        CollectionAssert.AreEqual(new[] { 1 }, view);
+                        var expected = new List<EventArgs>
+                        {
+                            CachedEventArgs.CountPropertyChanged,
+                            CachedEventArgs.IndexerPropertyChanged,
+                            Diff.CreateAddEventArgs(1, 0),
+                            CachedEventArgs.GetOrCreatePropertyChangedEventArgs("Source"),
+                        };
+                        CollectionAssert.AreEqual(expected, actual, EventArgsComparer.Default);
+
+                        with.Value = new[] { 1, 2 };
+                        CollectionAssert.AreEqual(new[] { 1, 2 }, view);
+                        expected.AddRange(
+                            new EventArgs[]
+                            {
+                                CachedEventArgs.CountPropertyChanged,
+                                CachedEventArgs.IndexerPropertyChanged,
+                                Diff.CreateAddEventArgs(2, 1),
+                                CachedEventArgs.GetOrCreatePropertyChangedEventArgs("Source"),
+                            });
+                        CollectionAssert.AreEqual(expected, actual, EventArgsComparer.Default);
+                    }
+                }
+            }
+
+            [Test]
+            public void ObserveValueAsReadOnlyViewWhenArrayOfT()
+            {
+                var with = new With<int[]>();
+                using (var view = with.ObserveValue(x => x.Value).Select(x => (IMaybe<int[]>)x).AsReadOnlyView())
                 {
                     using (var actual = view.SubscribeAll())
                     {
