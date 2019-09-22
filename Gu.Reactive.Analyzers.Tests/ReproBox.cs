80b4d0b47e5836c7695ec1e35ba435c84a430b94
JohanLarsson <johan_gavle1@hotmail.com>
2019-09-22 14:02:47 +0200

    ReproBox

---
+++ Gu.Reactive.Analyzers.Tests/ReproBox.cs
@@ -0,0 +1,47 @@
+namespace Gu.Reactive.Analyzers.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using NUnit.Framework;
+
+    [Explicit]
+    internal class ReproBox
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static readonly IReadOnlyList<DiagnosticAnalyzer> AllAnalyzers =
+            typeof(KnownSymbol).Assembly.GetTypes()
+                               .Where(typeof(DiagnosticAnalyzer).IsAssignableFrom)
+                               .Select(t => (DiagnosticAnalyzer)Activator.CreateInstance(t))
+                               .ToArray();
+
+        private static readonly Solution Solution = CodeFactory.CreateSolution(
+            new FileInfo("C:\\Git\\Gu.State\\Gu.State.sln"),
+            AllAnalyzers,
+            MetadataReferences.FromAttributes());
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public void SolutionRepro(DiagnosticAnalyzer analyzer)
+        {
+            RoslynAssert.Valid(analyzer, Solution);
+        }
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public void Repro(DiagnosticAnalyzer analyzer)
+        {
+            var code = @"
+namespace N
+{
+    public sealed class C
+    {
+    }
+}";
+
+            RoslynAssert.Valid(analyzer, code);
+        }
+    }
+}
