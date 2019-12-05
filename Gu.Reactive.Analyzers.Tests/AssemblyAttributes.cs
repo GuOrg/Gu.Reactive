3bd43f3dedc1c15ff5a6332dee46a6d3ff0de249
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-05 15:28:25 +0100

    Amend.

--- Gu.Reactive.Analyzers.Tests/AssemblyAttributes.cs
+++ Gu.Reactive.Analyzers.Tests/AssemblyAttributes.cs
@@ -1,18 +1,3 @@
 using Gu.Roslyn.Asserts;
 
-
-[assembly: MetadataReference(typeof(object), new[] { "global", "mscorlib" })]
-[assembly: MetadataReference(typeof(System.Diagnostics.Debug), new[] { "global", "System" })]
-[assembly: MetadataReferences(
-    typeof(System.Linq.Enumerable),
-    typeof(System.Net.WebClient),
-    typeof(System.Reactive.Disposables.SerialDisposable),
-    typeof(System.Reactive.Disposables.ICancelable),
-    typeof(System.Reactive.Linq.Observable),
-    typeof(Gu.Reactive.Condition),
-    typeof(Gu.Wpf.Reactive.ConditionControl),
-    typeof(System.Xml.Serialization.XmlSerializer),
-    typeof(System.Windows.Media.Matrix),
-    typeof(Microsoft.CodeAnalysis.CSharp.CSharpCompilation),
-    typeof(Microsoft.CodeAnalysis.Compilation),
-    typeof(NUnit.Framework.Assert))]
+[assembly:TransitiveMetadataReferences(typeof(Gu.Wpf.Reactive.ConditionControl))]
