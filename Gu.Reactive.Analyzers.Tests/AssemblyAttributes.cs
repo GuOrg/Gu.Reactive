e992dc99e2dd41d5500645f7638c960b9b134991
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-05 15:13:21 +0100

    Use nuget in analyzer.

--- Gu.Reactive.Analyzers.Tests/AssemblyAttributes.cs
+++ Gu.Reactive.Analyzers.Tests/AssemblyAttributes.cs
@@ -1,23 +1,6 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
 using Gu.Roslyn.Asserts;
 
-[assembly: AssemblyTitle("Gu.Reactive.Analyzers.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gu.Reactive.Analyzers.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-[assembly: ComVisible(false)]
-
-[assembly: Guid("f154810d-9d97-4efe-8a86-1312a780802c")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: MetadataReference(typeof(object), new[] { "global", "mscorlib" })]
 [assembly: MetadataReference(typeof(System.Diagnostics.Debug), new[] { "global", "System" })]
 [assembly: MetadataReferences(
