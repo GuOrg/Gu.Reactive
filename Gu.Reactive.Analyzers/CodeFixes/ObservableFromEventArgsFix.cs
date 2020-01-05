3cd169d796f9832c60b585b9bb655be7277d661f
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-05 12:39:53 +0100

    Simplify.

--- Gu.Reactive.Analyzers/CodeFixes/ObservableFromEventArgsFix.cs
+++ Gu.Reactive.Analyzers/CodeFixes/ObservableFromEventArgsFix.cs
@@ -36,13 +36,36 @@ namespace Gu.Reactive.Analyzers
                                 x.Arguments[0],
                                 new[]
                                 {
-                                    x.Arguments[0]
-                                     .WithExpression(SyntaxFactory.ParseExpression("h => (_, e) => h(e)"))
+                                    Argument()
                                      .WithLeadingTrivia(x.Arguments[0].GetLeadingTrivia())
                                      .WithAdditionalAnnotations(Formatter.Annotation),
                                 })),
                         nameof(ObservableFromEventArgsFix),
                         diagnostic);
+
+                    ArgumentSyntax Argument()
+                    {
+                        return SyntaxFactory.Argument(
+                            SyntaxFactory.SimpleLambdaExpression(
+                                parameter: SyntaxFactory.Parameter(SyntaxFactory.Identifier("h")),
+                                body: SyntaxFactory.ParenthesizedLambdaExpression(
+                                    parameterList: SyntaxFactory.ParameterList(
+                                        parameters: SyntaxFactory.SeparatedList(
+                                            new[]
+                                            {
+                                                SyntaxFactory.Parameter(identifier: SyntaxFactory.Identifier(text: "_")),
+                                                SyntaxFactory.Parameter(identifier: SyntaxFactory.Identifier(text: "e")),
+                                            },
+                                            new[]
+                                            {
+                                                SyntaxFactory.Token(kind: SyntaxKind.CommaToken),
+                                            })),
+                                    body: SyntaxFactory.InvocationExpression(
+                                        expression: SyntaxFactory.IdentifierName("h"),
+                                        argumentList: SyntaxFactory.ArgumentList(
+                                            arguments: SyntaxFactory.SingletonSeparatedList<ArgumentSyntax>(
+                                                SyntaxFactory.Argument(SyntaxFactory.IdentifierName("e"))))))));
+                    }
                 }
             }
         }
