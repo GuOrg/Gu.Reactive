9d98529af9b327d7a9f6abe9f175d2a48ba4faa4
JohanLarsson <johan_gavle1@hotmail.com>
2021-02-27 11:21:16 +0100

    Fix warning.

--- Gu.Reactive.Tests/NotifyCollectionChangedExt/ObservePropertyChangedWithValueChainedItemPropertyChanged.cs
+++ Gu.Reactive.Tests/NotifyCollectionChangedExt/ObservePropertyChangedWithValueChainedItemPropertyChanged.cs
@@ -130,7 +130,7 @@ namespace Gu.Reactive.Tests.NotifyCollectionChangedExt
 
                 fake.Collection.Add(new Fake { Next = new Level { Name = "3" } });
                 Assert.AreEqual(7, changes.Count);
-                EventPatternAssert.AreEqual(fake.Collection[2], fake.Collection, fake.Collection[2].Next, Maybe.Some("3"), string.Empty, changes.Last());
+                EventPatternAssert.AreEqual(fake.Collection[2], fake.Collection, fake.Collection[2].Next, Maybe.Some<string?>("3"), string.Empty, changes.Last());
             }
 
             Assert.AreEqual(7, changes.Count);
