42feff385e65636200f9334f1594a804d58d04c5
JohanLarsson <johan_gavle1@hotmail.com>
2021-03-05 10:22:04 +0100

    Suppress CS8620 when Maybe<T>

---
+++ Gu.Reactive.Analyzers.Tests/Cs8620SuppressorTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Gu.Reactive.Analyzers.Tests
+{
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using NUnit.Framework;
+
+    public static class Cs8620SuppressorTests
+    {
+        private static readonly DiagnosticAnalyzer Analyzer = new Cs8620Suppressor();
+
+        [TestCase("M(IObservable<Maybe<ObservableCollection<C>>> observable) => observable.ItemPropertyChanged(x => x.P)")]
+        [TestCase("M(IObservable<Maybe<ObservableCollection<C>>> observable) => NotifyCollectionChangedExt.ItemPropertyChanged(observable, x => x.P)")]
+        public static void Suppresses(string text)
+        {
+            var code = @"
+#nullable enable
+namespace N
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using Gu.Reactive;
+
+    public class C : INotifyPropertyChanged
+    {
+        private string? p;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string? P
+        {
+            get => this.p;
+            set
+            {
+                if (value == this.p)
+                {
+                    return;
+                }
+
+                this.p = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public static object M(IObservable<Maybe<ObservableCollection<C>>> observable) => observable.ItemPropertyChanged(x => x.P);
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}".AssertReplace("M(IObservable<Maybe<ObservableCollection<C>>> observable) => observable.ItemPropertyChanged(x => x.P)", text);
+            RoslynAssert.Valid(Analyzer, code);
+        }
+    }
+}
---
+++ Gu.Reactive.Analyzers/Suppressors/Cs8620Suppressor.cs
@@ -0,0 +1,37 @@
+﻿namespace Gu.Reactive.Analyzers
+{
+    using System.Collections.Immutable;
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class Cs8620Suppressor : DiagnosticSuppressor
+    {
+        private static readonly SuppressionDescriptor Descriptor = new SuppressionDescriptor(
+            nameof(Cs8620Suppressor),
+            "CS8620",
+            "No way to declare out T in structs and classes.");
+
+        public override ImmutableArray<SuppressionDescriptor> SupportedSuppressions { get; } = ImmutableArray.Create(
+            Descriptor);
+
+        public override void ReportSuppressions(SuppressionAnalysisContext context)
+        {
+            foreach (var diagnostic in context.ReportedDiagnostics)
+            {
+                if (diagnostic.GetMessage(CultureInfo.InvariantCulture) is { } message &&
+                    IsMaybe(message))
+                {
+                    context.ReportSuppression(Suppression.Create(Descriptor, diagnostic));
+                }
+
+                static bool IsMaybe(string message)
+                {
+                    return Regex.IsMatch(message, "Argument of type '.*Maybe<.+>>*' cannot be used for parameter '[^']+' of type '.*Maybe<.+\\?>>*'");
+                }
+            }
+        }
+    }
+}
