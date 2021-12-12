dce1a479a178a1de08c31432ced6b3b8ed36311c
JohanLarsson <johan_gavle1@hotmail.com>
2021-12-12 15:58:57 +0100

    Fix warnings

--- Gu.Reactive.Analyzers/Descriptors.cs
+++ Gu.Reactive.Analyzers/Descriptors.cs
@@ -4,7 +4,7 @@
 
     internal static class Descriptors
     {
-        internal static readonly DiagnosticDescriptor GUREA01DoNotObserveMutableProperty = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA01DoNotObserveMutableProperty = new(
             id: "GUREA01",
             title: "Don't observe mutable property.",
             messageFormat: "Don't observe mutable property.",
@@ -12,7 +12,7 @@
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true);
 
-        internal static readonly DiagnosticDescriptor GUREA02ObservableAndCriteriaMustMatch = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA02ObservableAndCriteriaMustMatch = new(
             id: "GUREA02",
             title: "Observable and criteria must match.",
 #pragma warning disable SA1118 // Parameter should not span multiple lines
@@ -28,7 +28,7 @@
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true);
 
-        internal static readonly DiagnosticDescriptor GUREA03PathMustNotify = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA03PathMustNotify = new(
             id: "GUREA03",
             title: "Path must notify.",
             messageFormat: "Path must notify.",
@@ -36,7 +36,7 @@
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true);
 
-        internal static readonly DiagnosticDescriptor GUREA04PreferSlimOverload = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA04PreferSlimOverload = new(
             id: "GUREA04",
             title: "Prefer slim overload.",
             messageFormat: "Prefer slim overload.",
@@ -44,7 +44,7 @@
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true);
 
-        internal static readonly DiagnosticDescriptor GUREA05FullPathMustHaveMoreThanOneItem = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA05FullPathMustHaveMoreThanOneItem = new(
             id: "GUREA05",
             title: "Full path must have more than one item.",
             messageFormat: "Full path must have more than one item.",
@@ -52,7 +52,7 @@
             defaultSeverity: DiagnosticSeverity.Error,
             isEnabledByDefault: true);
 
-        internal static readonly DiagnosticDescriptor GUREA06DoNotNewCondition = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA06DoNotNewCondition = new(
             id: "GUREA06",
             title: "Prefer injecting conditions.",
             messageFormat: "Prefer injecting conditions.",
@@ -60,7 +60,7 @@
             defaultSeverity: DiagnosticSeverity.Hidden,
             isEnabledByDefault: false);
 
-        internal static readonly DiagnosticDescriptor GUREA07DoNotNegateCondition = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA07DoNotNegateCondition = new(
             id: "GUREA07",
             title: "Prefer injecting conditions.",
             messageFormat: "Prefer injecting conditions.",
@@ -68,7 +68,7 @@
             defaultSeverity: DiagnosticSeverity.Hidden,
             isEnabledByDefault: false);
 
-        internal static readonly DiagnosticDescriptor GUREA08InlineSingleLine = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA08InlineSingleLine = new(
             id: "GUREA08",
             title: "Inline single line.",
             messageFormat: "Inline single line.",
@@ -76,7 +76,7 @@
             defaultSeverity: DiagnosticSeverity.Hidden,
             isEnabledByDefault: false);
 
-        internal static readonly DiagnosticDescriptor GUREA09ObservableBeforeCriteria = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA09ObservableBeforeCriteria = new(
             id: "GUREA09",
             title: "Place observable before criteria.",
             messageFormat: "Place observable before criteria.",
@@ -84,7 +84,7 @@
             defaultSeverity: DiagnosticSeverity.Hidden,
             isEnabledByDefault: false);
 
-        internal static readonly DiagnosticDescriptor GUREA10DoNotMergeInObservable = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA10DoNotMergeInObservable = new(
             id: "GUREA10",
             title: "Split up into two conditions?",
             messageFormat: "Split up into two conditions?",
@@ -92,7 +92,7 @@
             defaultSeverity: DiagnosticSeverity.Hidden,
             isEnabledByDefault: false);
 
-        internal static readonly DiagnosticDescriptor GUREA11PreferObservableFromEvent = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA11PreferObservableFromEvent = new(
             id: "GUREA11",
             title: "Prefer observing",
             messageFormat: "Prefer observing",
@@ -100,7 +100,7 @@
             defaultSeverity: DiagnosticSeverity.Hidden,
             isEnabledByDefault: true);
 
-        internal static readonly DiagnosticDescriptor GUREA12ObservableFromEventDelegateType = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA12ObservableFromEventDelegateType = new(
             id: "GUREA12",
             title: "Use correct delegate type.",
             messageFormat: "Use correct delegate type.",
@@ -108,7 +108,7 @@
             defaultSeverity: DiagnosticSeverity.Error,
             isEnabledByDefault: true);
 
-        internal static readonly DiagnosticDescriptor GUREA13SyncParametersAndArgs = new DiagnosticDescriptor(
+        internal static readonly DiagnosticDescriptor GUREA13SyncParametersAndArgs = new(
             id: "GUREA13",
             title: "Parameters and arguments for base initializer should have the same order.",
             messageFormat: "Parameters and arguments for base initializer should have the same order.",
